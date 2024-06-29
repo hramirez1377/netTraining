f521a2dba39b54f53389430e1de20eb849933e8a
Hernan Ramirez <hernan.ramirez@smartqs.com>
2024-06-28 23:40:59 -0600

    my firts commit

---
+++ CursoMaui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CursoMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
